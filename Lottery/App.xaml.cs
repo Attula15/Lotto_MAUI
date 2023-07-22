7de5e8f5d7c9d22222a8a6aa63d8c6c5f01c9281
Attula15 <100715516+Attula15@users.noreply.github.com>
2023-07-22 19:01:36 +0200

    init commit

---
+++ Lottery/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Lottery;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Lottery/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Lottery;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
