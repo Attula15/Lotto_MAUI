58103540ee0ffa06babf33d221c8c38727b3e250
Attula15 <100715516+Attula15@users.noreply.github.com>
2023-07-22 19:26:19 +0200

    Working base project

--- Lottery/MauiProgram.cs
+++ Lottery/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Lottery.ViewModel;
+using Microsoft.Extensions.Logging;
 
 namespace Lottery;
 
@@ -14,6 +15,9 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton<MainPageViewModel>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
---
+++ Lottery/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Lottery.ViewModel;
+
+public partial class MainPageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private String name = "";
+
+    [ObservableProperty]
+    private String tempName = "";
+
+    [ObservableProperty]
+    private bool visible = true;
+
+    [RelayCommand]
+    public void welcomeMessage()
+    {
+        Name = TempName;
+    }
+}
+
